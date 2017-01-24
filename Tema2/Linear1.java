3867e1da429b7dac637356be43621b2c56a19a09
Annika Svedin <annika.svedin@gmail.com>
2017-01-24 13:47:37 +0100

    linear code example

---
+++ Tema2/Linear1.java
@@ -0,0 +1,12 @@
+public boolean equalTo(String a[], String b[]) {
+        if (a.length != b.length) {
+            return false;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            if (!(a[i].equals(b[i]))) {
+                return false;
+            }
+        }
+        return true;
+    }
